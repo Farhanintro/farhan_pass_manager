2770e67480cb6323aee55c9f493fc2cbef8a437f
farhanCodercheck <143864141+farhanCodercheck@users.noreply.github.com>
2023-11-01 20:10:08 +0530

    Update indexmain.js

--- indexmain.js
+++ indexmain.js
@@ -1,7 +1,7 @@
-require('dotenv').config()
+// require('dotenv').config()
 function sendkey() {
 
-    const SendDataKey = process.env.seckey;
+    const SendDataKey = 327426;
     // const mySendDataKey = parseInt(SendDataKey, 10)
     // console.log(mySendDataKey);
     const secure_key = document.getElementById("secure_key").value;
